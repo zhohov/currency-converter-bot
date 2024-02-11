2d81195c801d595c984ce7760093c06a6ca9c0d5
maksimzhohov <zhohovmaksim@gmail.com>
2024-02-12 01:58:41 +0300

    Added help handler

--- bot/src/handlers/user_interaction_handlers/__init__.py
+++ bot/src/handlers/user_interaction_handlers/__init__.py
@@ -1,8 +1,10 @@
 from aiogram import Router
 from aiogram.filters import Command
 
+from .help import help_command
 from .start import start_command
 
 
 def register_user_interaction_handlers(router: Router) -> None:
     router.message.register(start_command, Command(commands=["start"]))
+    router.message.register(help_command, Command(commands=["help"]))
